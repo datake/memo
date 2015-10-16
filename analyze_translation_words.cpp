4059405e6a7605ce059eb41525e3d5f78df12bd8
datake <sigur.ku@gmail.com>
2015-10-16 16:44:47 +0900

    A->Bの辞書からB->AのデータにしてCSV出力

--- analyze_translation_words.cpp
+++ analyze_translation_words.cpp
@@ -13,17 +13,18 @@ using namespace std;
 int read_data();
 int dump_data(map<string, vector<string> > map_S_VS);
 int write_head_trans_csv(map<string, vector<string> > map_S_VS, char* outputfilename_char);
-int write_trans_times_csv(map<string, int > map_S_I, char* outputfilename_char);
+int write_trans_heads_csv(map<string, vector<string> > map_S_VS, char* outputfilename_char);
 
 int main(){
 	int i=1;
 	string filename;
 	vector<string> VS;
+	vector<string> VS_tmp;
 	vector<string> ::iterator vecitr;
-	map<string, vector<string> > map_S_VS;//見出し語と訳語のvector
+	map<string, vector<string> > map_S_VS;//見出し語とそれに対応する訳語
 	map<string, vector<string> > ::iterator mapitr;
-	map<string, int> map_S_I;//訳語と訳語の出現回数
-	map<string, int> ::iterator mapitr2;
+	map<string, vector<string> > map_S_VS2;//訳語とそれに対応する見出し語
+	map<string, vector<string> > ::iterator mapitr2;
 	string str;
 	string line;
 	string str1,str2,str3,str4,str5;
@@ -32,9 +33,24 @@ int main(){
 	string val;
 	int cinnumber;
 	cout<<"select -> 1:Ja_En.csv"<<endl;
+	cout<<"select -> 2:En_Ja.csv"<<endl;
+	cout<<"select -> 3:De_En.csv"<<endl;
+	cout<<"select -> 4:En_De.csv"<<endl;
+	cout<<"select -> 5:Ja_De.csv"<<endl;
+	cout<<"select -> 6:De_Ja.csv"<<endl;
 	cin>>cinnumber;
 	if(cinnumber==1){
 		filename="Ja_En.csv";
+	}else if(cinnumber==2){
+		filename="En_Ja.csv";
+	}else if(cinnumber==3){
+		filename="De_En.csv";
+	}else if(cinnumber==4){
+		filename="En_De.csv";
+	}else if(cinnumber==5){
+		filename="Ja_De.csv";
+	}else if(cinnumber==6){
+		filename="De_Ja.csv";
 	}
 	const char* filename_char=filename.c_str();
 
@@ -48,8 +64,8 @@ int main(){
 	char outputfilename_char[30] = "output/output_";
 	strcat(outputfilename_char, filename_char);
 	//訳語とその回数のファイル出力用
-	char output_trans_times_char[30] = "output/trans_times_";
-	strcat(output_trans_times_char, filename_char);
+	char output_trans_heads_char[30] = "output/trans_heads_";
+	strcat(output_trans_heads_char, filename_char);
 
 
 	if(ifs.fail()) {
@@ -83,15 +99,17 @@ int main(){
 					//ofs<<val;
 					VS.push_back(val);//訳語をvectorに積む
 
-					mapitr2 = map_S_I.find(val);
-					if (mapitr2 == map_S_I.end() ) {
+					mapitr2 = map_S_VS2.find(val);
+					if (mapitr2 == map_S_VS2.end() ) {
 						//もしその訳語が未登録なら追加
 						std::cout << "not found:" <<val<< std::endl;
-						map_S_I[val]=1;
+						VS_tmp.clear();
+						VS_tmp.push_back(key);
+						map_S_VS2[val]=VS_tmp;
 					} else {
 						//もしその訳語がすでに登録されていたら回数を+1
-						mapitr2->second++;
-						cout<<"found translation word:"<<mapitr2->first<<",times:"<<mapitr2->second<<endl;
+						mapitr2->second.push_back(key);
+						cout<<"found translation word:"<<mapitr2->first<<",ex:"<<mapitr2->second.back()<<endl;
 					}
 				}
 			}
@@ -121,8 +139,8 @@ int main(){
 
 
 	//dump_data(map_S_VS);
-	//write_head_trans_csv(map_S_VS, outputfilename_char);
-	write_trans_times_csv(map_S_I, output_trans_times_char);
+	write_head_trans_csv(map_S_VS, outputfilename_char);
+	write_trans_heads_csv(map_S_VS2, output_trans_heads_char);
 
 	return 0;
 }
@@ -161,15 +179,22 @@ int write_head_trans_csv(map<string, vector<string> > map_S_VS, char* outputfile
 		return 0;
 }
 
-int write_trans_times_csv(map<string, int > map_S_I, char* outputfilename_char){
-	map<string, int > ::iterator mapitr;
+int write_trans_heads_csv(map<string, vector<string> > map_S_VS, char* outputfilename_char){
+	map<string, vector<string> > ::iterator mapitr;
 	ofstream ofs(outputfilename_char);
-	for (mapitr = map_S_I.begin(); mapitr != map_S_I.end(); mapitr++){
-		// firstには訳語,secondには複数の訳語の出現回数
-		ofs << mapitr->first<<"," << mapitr->second<<"\n";
+	for (mapitr = map_S_VS.begin(); mapitr != map_S_VS.end(); mapitr++){
+		// firstには訳語,secondには複数の訳語
+		ofs << mapitr->first<<",";
+		for(int i=0;i<mapitr->second.size();i++){
 
+			if(i==(mapitr->second.size()-1)){
+				ofs<< mapitr->second[i]<<"\n";
+			}else{
+				ofs<< mapitr->second[i]<<",";
+			}
+		}
 	}
 	cout<<"finished wrote "<<outputfilename_char<<endl;
-	cout<<"number of headwords="<<map_S_I.size()<<endl;
+	cout<<"number of headwords="<<map_S_VS.size()<<endl;
 	return 0;
 }
