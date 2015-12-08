619ab4283cf64ffae44826d588f2dd1664d61b69
datake <sigur.ku@gmail.com>
2015-12-09 07:24:40 +0900

    マルダンで通過するトランスグラフの条件ミスを修正

--- partition_graph.py
+++ partition_graph.py
@@ -7,7 +7,9 @@ import metis
 import pydot
 from pprint import pprint
 
-vector = {}
+#トランスグラフのcsvデータを入力として、
+#もともと繋がっているトランスグラフデータに分けて
+#トランスグラフの数の分csv出力(切断はしない)
 
 G=nx.Graph()
 # which_lang="JaToEn_EnToDe"
@@ -22,61 +24,64 @@ for row in dataReader:
     # print row[0]
     G.add_node(row[0],lang='En')
     # G.graph[row[0]]='English'
-    vector[row[0]] = []
     row1_separate =row[1].split(',')
     row2_separate =row[2].split(',')
     for lang_a in row1_separate:
-        vector[row[0]].append(lang_a)
         G.add_node(lang_a,lang='Ja')
         G.add_edge(lang_a,row[0])
-        pprint(lang_a)
+        # pprint(lang_a)
 
     for lang_b in row2_separate:
-        vector[row[0]].append(lang_b)
         G.add_node(lang_b,lang='De')
         G.add_edge(lang_b,row[0])
-        pprint(lang_b)
+        # pprint(lang_b)
 
 f.close()
 
 
 graphs = nx.connected_component_subgraphs(G)
 
-i=0
+subgraph_count=0
+pass_subgraph_count=0
 
 for subgraph in graphs:
-    # print(G.adj)
-    print "*********************subgraph number:"+str(i)+"***************************"
+    lang=nx.get_node_attributes(subgraph,'lang') # <-この処理遅い
+    print "*********************subgraph number:"+str(pass_subgraph_count)+"("+str(subgraph_count)+")***************************"
+    subgraph_count+=1
     print "*********************subgraph node数:"+str(len(subgraph))+"***************************"
     # if len(subgraph)<30000: #大きいトランスグラフをとばす場合はコメントアウト外す
-    if 6 < len(subgraph): #!!!
-        with open(output_each_trans_filename+"_subgraph_"+str(i)+".csv", "w") as file:
+    if 6 < len(subgraph): #!!! ノード数の制限
+        # ピボット数の制限
+        pivot_count=0
+        for node in subgraph.nodes():
+            if lang[node]=='En':
+                pivot_count+=1
 
-            for node in subgraph.nodes():
-                ja_neighbors_pivot = set()
-                de_neighbors_pivot = set()
-                lang=nx.get_node_attributes(subgraph,'lang')
-                if lang[node]=='En':
-                    pprint(node)
-                    for node_ja_de in subgraph.neighbors(node):
-                        if lang[node_ja_de]=='Ja':
-                            ja_neighbors_pivot.add(node_ja_de)
-                        elif lang[node_ja_de]=='De':
-                            de_neighbors_pivot.add(node_ja_de)
+        if pivot_count > 1:
+            with open(output_each_trans_filename+"_subgraph_"+str(pass_subgraph_count)+".csv", "w") as file:
+                pass_subgraph_count+=1
+                for node in subgraph.nodes():
+                    ja_neighbors_pivot = set()
+                    de_neighbors_pivot = set()
+                    if lang[node]=='En':
+                        pprint(node)
+                        for node_ja_de in subgraph.neighbors(node):
+                            if lang[node_ja_de]=='Ja':
+                                ja_neighbors_pivot.add(node_ja_de)
+                            elif lang[node_ja_de]=='De':
+                                de_neighbors_pivot.add(node_ja_de)
 
-                    file.write("\""+node+"\",\"")
-                    last = len(ja_neighbors_pivot) - 1
-                    for i,ja in enumerate(ja_neighbors_pivot):
-                        if i == last:
-                            file.write(ja+"\",\"")
-                        else:
-                            file.write(ja+",")
+                        file.write("\""+node+"\",\"")
+                        last = len(ja_neighbors_pivot) - 1
+                        for i,ja in enumerate(ja_neighbors_pivot):
+                            if i == last:
+                                file.write(ja+"\",\"")
+                            else:
+                                file.write(ja+",")
 
-                    last = len(de_neighbors_pivot) - 1
-                    for i,de in enumerate(de_neighbors_pivot):
-                        if i == last:
-                            file.write(de+"\"\n")
-                        else:
-                            file.write(de+",")
-
-        i+=1
+                        last = len(de_neighbors_pivot) - 1
+                        for i,de in enumerate(de_neighbors_pivot):
+                            if i == last:
+                                file.write(de+"\"\n")
+                            else:
+                                file.write(de+",")
