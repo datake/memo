18a4f8047784191718dbfd723bf4f124f4c6a03c
datake <sigur.ku@gmail.com>
2016-01-19 06:52:00 +0900

    ノード数決めてグラフ作成(ノード数あってない)

--- generate_random_node.py
+++ generate_random_node.py
@@ -4,208 +4,168 @@ import csv
 from pprint import pprint
 import pygraphviz as pgv
 import matplotlib.pyplot as plt
+import os
 from datetime import datetime
 itr_count=0
-# while itr_count<100:
-#
-#     itr=[]
-#     weight = [1] * 10
-#     # weight = [0.2531605027,0.320774498,0.1713213992,0.08402242406,0.04440092051,0.02575478045,0.0149637045,0.01003017924,0.006021465853,0.004034759747,0.002254648857,0.00171581481,0.001427267648,0.00125859926,0.0006137083256,0.000406779661,0.0002711864407,0.0003389830508,0.00006779661017,0.0002033898305,0.0002711864407,0.00006779661017,0.0004103184951,0,0,0,0.00006779661017,0.00006779661017,0,0]
-#     weight = np.array(weight)
-#     weight = weight/weight.sum()
-#     for i in range(1,len(weight)+1):
-#         itr.append(i)
-#
-#     node_a = np.random.choice(itr,p=weight)
-#     node_b = np.random.choice(itr,p=weight)
-#     print("node_a:"+str(node_a))
-#     print("node_b:"+str(node_b))
-#
-#     itr=[]
-#     weight_pivot = [1] * 10
-#     # weight_pivot = [0,0.7768958627,0.1542562061,0.03509816081,0.01779913524,0.007474975851,0.002426599238,0.001526246866,0.002923928604,0.0002711864407,0.0006497320378,0.0001355932203,0,0.0001355932203,0,0.0001355932203,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-#     weight_pivot = np.array(weight_pivot)
-#     weight_pivot = weight_pivot/weight_pivot.sum()
-#     for i in range(1,len(weight_pivot)+1):
-#         itr.append(i)
-#     #重み付きランダム(ノード)
-#     node_p = np.random.choice(itr,p=weight_pivot)
-#
-#     print("p:"+str(node_p))
-#     itr=[]
-#     weight_edge = [1] * 15
-#     # weight_edge = [0.3783812933,0.2521690888,0.1451887865,0.06993896472,0.05019433731,0.02662702045,0.0161860435,0.01261468098,0.008239611842,0.006692367802,0.004658431193,0.003966963137,0.00233053026,0.001341815171,0.001616245302,0.001051670595,0.0009350355104,0.0009523962318,0.0002033898305,0.0002033898305,0.0002033898305,0.0002033898305,0.0005423728814,0.0002711864407,0,0.00006779661017,0.0002033898305,0.00006779661017,0,0.0002570694087,0.0001355932203,0.00006779661017,0,0.00006779661017]
-#     weight_edge_a=np.array(weight_edge[node_a:-1])
-#     weight_edge_b=np.array(weight_edge[node_b:-1])
-#     weight_edge_a = weight_edge_a/weight_edge_a.sum()
-#
-#     weight_edge_b = weight_edge_b/weight_edge_b.sum()
-#
-#     itr_a=[]
-#     itr_b=[]
-#     # weight_edge = np.array(weight_edge)
-#     # weight_edge = weight_edge/weight_edge.sum()
-#     for i in range(node_a,len(weight_edge_a)+1):
-#         itr_a.append(i)
-#
-#     for i in range(node_b,len(weight_edge_b)+1):
-#         itr_b.append(i)
-#
-#
-#     #重み付きランダム(ノード)
-#     while True:
-#         # edge_ap = np.random.choice(itr_a,p=weight_edge_a)
-#         # edge_bp = np.random.choice(itr_b,p=weight_edge_b)
-#         edge_ap=np.random.randint(np.max([node_a,node_p]), node_a+node_p)  # Integer from 1 to 10, endpoints included
-#         edge_bp=np.random.randint(np.max([node_b,node_p]), node_b+node_p)  # Integer from 1 to 10, endpoints included
-#         pprint(node_a)
-#         pprint(node_b)
-#         pprint(node_p)
-#
-#         print("edge_ap:"+str(edge_ap))
-#         print("edge_bp:"+str(edge_bp))
-#
-#         condition=  edge_ap>= node_a and edge_bp>= node_b and edge_ap>= node_p and edge_bp>= node_p #ノード数とエッジの関係
-#         condition = condition and (node_a+node_p)>=edge_ap and (node_b+node_p)>=edge_bp #これ以上はる枝がない
-#
-#         if condition:
-#             break
-#
-# """
+
 n=5
-for node_a in range(1,n+1):
-    for node_b in range(1,n+1):
+output_directory="generate_transgraph/graph/"
+
+for node_a in range(1,n):
+    for node_b in range(1,n):
         for node_p in range(1,5):
             # while True:
             for edge_ap in range(np.max([node_a,node_p]), node_a*node_p):
                 for edge_bp in range(np.max([node_b,node_p]), node_b*node_p):
-
-                    # edge_ap=np.random.randint(np.max([node_a,node_p]), node_a+node_p)  # Integer from 1 to 10, endpoints included
-                    # edge_bp=np.random.randint(np.max([node_b,node_p]), node_b+node_p)  # Integer from 1 to 10, endpoints included
-                    pprint(node_a)
-                    pprint(node_b)
-                    pprint(node_p)
+                    print("a:"+str(node_a))
+                    print("b:"+str(node_b))
+                    print("p:"+str(node_p))
 
                     print("edge_ap:"+str(edge_ap))
                     print("edge_bp:"+str(edge_bp))
-                    #
-                    # condition=  edge_ap>= node_a and edge_bp>= node_b and edge_ap>= node_p and edge_bp>= node_p #ノード数とエッジの関係
-                    # condition = condition and (node_a+node_p)>=edge_ap and (node_b+node_p)>=edge_bp #これ以上はる枝がない
-                    #
-                    # if condition:
-                    #     break
 
-
-                    output_filename="generate_transgraph/"
                     condition=  edge_ap>= node_a and edge_bp>= node_b and edge_ap>= node_p and edge_bp>= node_p #ノード数とエッジの関係
                     condition = condition and (node_a*node_p)>=edge_ap and (node_b*node_p)>=edge_bp #これ以上はる枝がない
                     if condition:
                         G=nx.DiGraph()
 
                         # ピボット作成
-                        for i in range(1,node_p+1):
-                            G.add_node("p-"+str(i),lang='P',bipartite=2)
+                        # for i in range(1,node_p+1):
+                        for i in range(node_p):
+                            G.add_node("p-"+str(i),lang='language_P',langP='1',langA='0',langB='0')
                             print("p-"+str(i))
                             if node_a>node_p:
-                                G.add_edge("a-"+str(i),"p-"+str(i),is_edged=1)
+                                G.add_edge("a-"+str(i),"p-"+str(i))
                             if node_b>node_p:
-                                G.add_edge("p-"+str(i),"b-"+str(i),is_edged=1)
+                                G.add_edge("p-"+str(i),"b-"+str(i))
 
                         #ノード作成
-                        for i in range(1,node_a+1):
-                            G.add_node("a-"+str(i),lang='A',has_edge_a=0,bipartite=0)
+                        # for i in range(1,node_a+1):
+                        for i in range(node_a):
+                            G.add_node("a-"+str(i),lang='language_A',langA='1',langB='0')
                             print("a-"+str(i))
                             # A-Pノード
                             if node_p>node_a:
-                                G.add_edge("a-"+str(i),"p-"+str(i),is_edged=1)
+                                G.add_edge("a-"+str(i),"p-"+str(i))
+                                print("hgoe")
 
 
-                        for i in range(1,node_b+1):
-                            G.add_node("b-"+str(i),lang='B',has_edge_b=0,bipartite=1)
+                        # for i in range(1,node_b+1):
+                        for i in range(node_b):
+                            G.add_node("b-"+str(i),lang='language_B',langB='1',langA='0')
                             print("b-"+str(i))
                             # B-Pノード
                             if node_p>node_b:
-                                G.add_edge("p-"+str(i),"b-"+str(i),is_edged=1)
+                                G.add_edge("p-"+str(i),"b-"+str(i))
+                                print("hgoe")
+
                         tmp=0
                         while True:
                             tmp+=1
-                            #必ずはる
-                            # if tmp<edge_ap:
-                            #     for i in range(1,np.min([node_a,node_p])+1):
-                            #         # tmp_a=np.random.random_integers(i)
-                            #         # tmp_p=np.random.random_integers(i)
-                            #         G.add_edge("a-"+str(i),"p-"+str(i),is_edged=1)
-                            #         # print("p-a"+str(i))
-                            #
-                            #     for i in range(1,np.min([node_b,node_p])+1):
-                            #
-                            #         # tmp_b=np.random.random_integers(i)
-                            #         # tmp_p=np.random.random_integers(i)
-                            #         G.add_edge("p-"+str(i),"b-"+str(i),is_edged=1)
-                            #         # print("p-b"+str(i))
-                            #枝はる
-                            # edge_ap=np.random.randint(np.max([node_a,node_p]), node_a+node_p)  # Integer from 1 to 10, endpoints included
-                            # edge_bp=np.random.randint(np.max([node_b,node_p]), node_b+node_p)  # Integer from 1 to 10, endpoints included
-                            tmp_a=np.random.random_integers(node_a)
-                            tmp_p=np.random.random_integers(node_p)
-                            tmp_b=np.random.random_integers(node_b)
+
+                            # tmp_a=np.random.random_integers(node_a)
+                            # tmp_p=np.random.random_integers(node_p)
+                            # tmp_b=np.random.random_integers(node_b)
+                            tmp_a=np.random.randint(node_a-1)
+                            tmp_p=np.random.randint(node_p-1)
+                            tmp_b=np.random.randint(node_b-1)
 
                             #AとP
-                            for i in range(np.min([node_a,node_p]),edge_ap+1):
-                                tmp_a=np.random.random_integers(node_a)
-                                tmp_p=np.random.random_integers(node_p)
-                                tmp_b=np.random.random_integers(node_b)
+                            for i in range(np.min([node_a,node_p]),edge_ap):
+                            # for i in range(np.min([node_a,node_p]),np.max([node_a,node_p])):
+
                                 if node_a > node_p: #P少ない
-                                    G.add_edge("a-"+str(i),"p-"+str(tmp_p),is_edged=1)
+                                    G.add_edge("a-"+str(i),"p-"+str(tmp_p))
                                 else: #A少ない
                                     # for i in range(np.min([node_a,node_p])+1,edge_ap+1):
-                                    G.add_edge("a-"+str(tmp_a),"p-"+str(i),is_edged=1)
+                                    G.add_edge("a-"+str(tmp_a),"p-"+str(i))
 
                             #BとP
-                            # if tmp<edge_bp:
-                            for i in range(np.min([node_b,node_p]),edge_bp+1):
-                                tmp_a=np.random.random_integers(node_a)
-                                tmp_p=np.random.random_integers(node_p)
-                                tmp_b=np.random.random_integers(node_b)
+                            for i in range(np.min([node_b,node_p]),edge_bp):
+                            # for i in range(np.min([node_b,node_p]),np.max([node_b,node_p])):
                                 if node_b > node_p: #P少ない
                                     # for i in range(np.min([node_b,node_p])+1,edge_bp+1):
-                                    G.add_edge("p-"+str(i),"b-"+str(node_b),is_edged=1)
+                                    G.add_edge("p-"+str(i),"b-"+str(tmp_b))
                                 else: #B少ない
                                     # for i in range(np.min([node_b,node_p])+1,edge_bp+1):
-                                    G.add_edge("p-"+str(node_p),"b-"+str(i),is_edged=1)
+                                    G.add_edge("p-"+str(tmp_p),"b-"+str(i))
 
 
                             # condition=nx.is_connected(G.to_undirected())
-                            if tmp>20:
-                                break
-
-                            # if condition:
-                            #     condition=condition and G.number_of_nodes()==(node_a+node_b+node_p) and G.number_of_edges()== (edge_ap+edge_bp)
-                            #     if condition:
-                            #         print("作成成功")
-                            #         # break
-                            #     else:
-                            #         print("グラフ作成できず153")
-                            #         # break
-                            #
-                            # else:
-                            #     print("グラフ作成できず155")
-                            #     continue
-
-
-                            if nx.is_connected(G.to_undirected()) and G.number_of_nodes()==(node_a+node_b+node_p) and G.number_of_edges()== (edge_ap+edge_bp):
-                                itr_count += 1
-                                g_visualize = nx.to_agraph(G)
-                                # g_visualize.draw(output_filename+"graph/"+str(node_a)+str(node_p)+str(node_b)+str(edge_ap)+str(edge_bp)+str(itr_count)+'.pdf',prog='dot')
-                                g_visualize.draw(output_filename+"graph/"+str(node_a)+str(node_p)+str(node_b)+str(edge_ap)+str(edge_bp)+str(itr_count)+datetime(2014,1,2,3,4,5).strftime('%s')+'.pdf',prog='dot')
-                            else:
+                            if tmp>50:
                                 print("グラフ作成できず163")
+
                                 break
 
-                            with open(output_filename+"csv/"+str(node_a)+str(node_p)+str(node_b)+str(edge_ap)+"-"+str(edge_bp)+str(itr_count)+".csv", "w") as file:
-                                itr_count+=1
-                                # lang=nx.get_node_attributes(G,'lang') # <-この処理遅い
-                                file.write("test")
+                            # 任意のピボットと必ずAとBはつながる
+                            # A,B,Pの個数確認
+                            count_connected_component=0
+                            condition=0
+                            has_edge_pa=0
+                            has_edge_pb=0
+                            output_node_a=0
+                            output_node_b=0
+                            output_node_p=0
+                            graphs = nx.connected_component_subgraphs(G.to_undirected())
+                            #
+                            for subgraph in graphs:
+                                count_connected_component+=1
+                                if count_connected_component==1:
+                                    lang=nx.get_node_attributes(subgraph,'lang')
+                                    langP=nx.get_node_attributes(subgraph,'langP')
+                                    langA=nx.get_node_attributes(subgraph,'langA')
+                                    langB=nx.get_node_attributes(subgraph,'langB')
+
+                                    # if len(subgraph)==(node_a+node_b+node_p):
+                                    for node in subgraph.nodes():
+                                        pprint("node")
+                                        pprint(node)
+                                        # print(langA[node])
+                                        # print(langB[node])
+
+                                        # print(lang[node])
+                                        # if lang[node]=='language_P':
+                                        #     pprint(node)
+                                        #     # has_edge_pa=0
+                                        #     # has_edge_pb=0
+                                        #     for node_a_b in subgraph.neighbors(node):
+                                        #         if lang[node_a_b]=='language_A':
+                                        #             has_edge_pa=1
+                                        #         elif lang[node_a_b]=='language_B':
+                                        #             has_edge_pb=1
+                                        #             # else
+                                                    #     has_edge_pb*=-1
+
+                                else:
+                                    print("一つのトランスグラフになってない")
+
+
+
+                            # output_node_a=
+                            # output_node_b=
+                            # output_node_p=
+                            if nx.is_connected(G.to_undirected()):# and has_edge_pa ==1 and has_edge_pb == 1:#and G.number_of_nodes()==(node_a+node_b+node_p) and G.number_of_edges()== (edge_ap+edge_bp):
+                                itr_count += 1
+                                print("祝作成")
+
+                                g_visualize = nx.to_agraph(G)
+                                # g_visualize.draw(output_directory+"graph/"+str(node_a)+str(node_p)+str(node_b)+str(edge_ap)+str(edge_bp)+str(itr_count)+'.pdf',prog='dot')
+                                output_new_dir=str(node_a)+"-"+str(node_p)+"-"+str(node_b)
+                                if not os.path.exists(output_directory+output_new_dir):
+                                    os.makedirs(output_directory+output_new_dir)
+
+                                output_file=str(node_a)+"-"+str(node_p)+"-"+str(node_b)+"-"+str(edge_ap)+"-"+str(edge_bp)+"-"+str(itr_count)+datetime(2014,1,2,3,4,5).strftime('%s')
+                                g_visualize.draw(output_directory+output_new_dir+"/"+output_file+'.pdf',prog='dot')
+                                # with open(output_directory+"csv/"+str(node_a)+str(node_p)+str(node_b)+str(edge_ap)+"-"+str(edge_bp)+str(itr_count)+".csv", "w") as file:
+                                    # itr_count+=1
+                                    # lang=nx.get_node_attributes(G,'lang') # <-この処理遅い
+                                    # file.write("test")
+                                # break
+                            # else:
+                                # tmp+=1
+                                # break
+
+
                     else:
                         print("グラフ作成できず")
                         continue
